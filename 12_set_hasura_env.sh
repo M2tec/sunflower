74f16f9772467b4728e2c37513b55ddd0e7036d1
Maarten Menheere <m.menheere@m2tec.nl>
2023-10-28 09:53:54 -0400

    Fixes2

--- 12_set_hasura_env.sh
+++ 12_set_hasura_env.sh
@@ -5,7 +5,7 @@ set -x
 echo "$PWD"
 
 echo "" >> ~/.bashrc
-echo "export CARDANO_NODE_CONFIG_PATH=\"${PWD}/cardano-node/mainnet/config.json\"" >> ~/.bashrc
+echo "export CARDANO_NODE_CONFIG_PATH=\"${PWD}/node-config/mainnet/config.json\"" >> ~/.bashrc
 echo "export HASURA_GRAPHQL_ENABLE_TELEMETRY=false" >> ~/.bashrc
 echo "export HASURA_GRAPHQL_SERVER_PORT=8081" >> ~/.bashrc
 echo "export HASURA_CLI_PATH=\"/usr/local/bin/hasura\"" >> ~/.bashrc
@@ -15,8 +15,8 @@ echo "export OGMIOS_HOST=\"localhost:1337\"" >> ~/.bashrc
 echo "export POSTGRES_HOST=\"localhost\"" >> ~/.bashrc
 echo "export POSTGRES_PORT=5432" >> ~/.bashrc
 echo "export POSTGRES_DB_FILE=\"${PWD}/postgres_db\"" >> ~/.bashrc
-echo "export POSTGRES_PASSWORD_FILE=\"${PWD}/dbsync-script/postgres_password\"" >> ~/.bashrc
-echo "export POSTGRES_USER_FILE=\"${PWD}/dbsync-script/postgres_user\"" >> ~/.bashrc
+echo "export POSTGRES_PASSWORD_FILE=\"${PWD}/postgres_password\"" >> ~/.bashrc
+echo "export POSTGRES_USER_FILE=\"${PWD}/postgres_user\"" >> ~/.bashrc
 echo "export NETWORK=\"mainnet\"" >> ~/.bashrc
 
 source ~/.bashrc
